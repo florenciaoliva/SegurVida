7a1450a0cc5c0df2a5a220e432f58363cbfbb243
Florencia Oliva <florolivaok@gmail.com>
2025-09-06 13:13:52 -0300

    initial convex auth setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,28 @@
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
 });
 
+const secureStorage = {
+  getItem: SecureStore.getItemAsync,
+  setItem: SecureStore.setItemAsync,
+  removeItem: SecureStore.deleteItemAsync,
+};
+
 export default function RootLayout() {
   return (
-    <ConvexProvider client={convex}>
+    <ConvexAuthProvider
+      client={convex}
+      storage={Platform.OS === "android" || Platform.OS === "ios" ? secureStorage : undefined}
+    >
       <Stack>
         <Stack.Screen name="index" />
       </Stack>
-    </ConvexProvider>
+    </ConvexAuthProvider>
   );
 }
