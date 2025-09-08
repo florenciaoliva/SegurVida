2a4e960525deb173a3f4fd22391c2a28620bac80
Florencia Oliva <florolivaok@gmail.com>
2025-09-08 16:31:43 -0300

    add app icons

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,5 +1,5 @@
 import { SignInWithPassword } from "@/features/auth/signin-with-password";
-import { SafeAreaView, View } from "react-native";
+import { Image, SafeAreaView, View } from "react-native";
 
 export default function SignIn() {
   const handleSent = (email: string) => {
@@ -13,18 +13,29 @@ export default function SignIn() {
 
   return (
     <SafeAreaView className="flex-1 bg-gray-100">
-      <View className="flex-1 justify-center">
-        <SignInWithPassword
-          handleSent={handleSent}
-          handlePasswordReset={handlePasswordReset}
-          passwordRequirements={{
-            minLength: 8,
-            requireUppercase: true,
-            requireLowercase: true,
-            requireNumber: true,
-            requireSpecialChar: false,
-          }}
+      <View className="flex-1">
+        {/* Background Image */}
+        <Image
+          source={require("@/assets/images/splash-icon.png")}
+          className="absolute inset-0 w-full h-full"
+          style={{ opacity: 0.1 }}
+          resizeMode="center"
         />
+        
+        {/* Content */}
+        <View className="flex-1 justify-center">
+          <SignInWithPassword
+            handleSent={handleSent}
+            handlePasswordReset={handlePasswordReset}
+            passwordRequirements={{
+              minLength: 8,
+              requireUppercase: true,
+              requireLowercase: true,
+              requireNumber: true,
+              requireSpecialChar: false,
+            }}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
